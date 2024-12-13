4b4aedf5da724d79a6273abdc6b2062079703423
videoqwg <3080926500@qq.com>
2024-12-13 21:36:08 +0800

    修改默认登录用户

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -60,17 +60,20 @@
 </template>
 
 <script>
-import { validUsername } from '@/utils/validate'
+// import { validUsername } from '@/utils/validate'
 
 export default {
   name: 'Login',
   data() {
     const validateUsername = (rule, value, callback) => {
+      /*
       if (!validUsername(value)) {
         callback(new Error('Please enter the correct user name'))
       } else {
         callback()
       }
+        */
+      callback()
     }
     const validatePassword = (rule, value, callback) => {
       if (value.length < 6) {
@@ -81,8 +84,8 @@ export default {
     }
     return {
       loginForm: {
-        username: 'admin',
-        password: '111111'
+        username: '123',
+        password: '123456'
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
