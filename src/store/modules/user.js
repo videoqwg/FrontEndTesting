75cfc6570055013b62bf0976d74997bf1891ad8f
videoqwg <3080926500@qq.com>
2025-02-11 17:04:45 +0800

    将获取用户信息的逻辑更改回原样，防止更新用户信息不及时，以及添加与信息相关的操作

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,8 @@
-import { login, register, logout, getInfo, updateUserData, updateAccountData } from '@/api/user'
+import { login, register, logout, getInfo, updateUserData, updateAccountData, getFriends, getGroups, addFriend } from '@/api/user'
 import { getToken, setToken, removeToken } from '@/utils/auth'
 import router, { resetRouter } from '@/router'
+import WebSocketSingleton from '@/utils/websocketInstance'
+// import { setEncryptedCookie, getDecryptedCookie, removeCookie } from '@/utils/EncryptedCookie'
 
 const getDefaultState = () => {
   return {
@@ -11,7 +13,9 @@ const getDefaultState = () => {
     introduction: '',
     phone: '',
     email: '',
-    roles: []
+    roles: [],
+    friends: [],
+    groups: []
   }
 }
 
@@ -44,10 +48,35 @@ const mutations = {
   },
   SET_EMAIL: (state, email) => {
     state.email = email
+  },
+  SET_FRIENDS: (state, friends) => {
+    state.friends = friends
+  },
+  SET_GROUPS: (state, groups) => {
+    state.groups = groups
   }
 }
 
 const actions = {
+
+  // 示例：添加消息到 messages 模块
+  addMessageToMessagesModule({ dispatch }, message) {
+  // 调用 messages 模块的 ADD_MESSAGE 方法
+    dispatch('messages/saveMessage', message, { root: true })
+  },
+
+  // 示例：加载 messages 模块的消息
+  loadMessagesFromMessagesModule({ dispatch }) {
+  // 调用 messages 模块的 loadMessages 方法
+    dispatch('messages/loadMessages', null, { root: true })
+  },
+
+  // 示例：同步消息
+  syncMessagesWithBackend({ dispatch }) {
+  // 调用 messages 模块的 syncWithBackend 方法
+    dispatch('messages/syncWithBackend', null, { root: true })
+  },
+
   // user login
   login({ commit }, userInfo) {
     const { username, password } = userInfo
@@ -84,31 +113,86 @@ const actions = {
   },
 
   // get user info
-  getInfo({ commit, state }) {
+
+  getInfo({ commit, dispatch }) {
     return new Promise((resolve, reject) => {
-      getInfo(state.token).then(response => {
-        const { data } = response
-        if (!data) {
-          return reject('Verification failed, please Login again.')
-        }
-        const { userid, roles, username, avatar, introduction, phone, email } = data
-        // 角色非空
-        if (!roles || roles.length <= 0) {
-          reject('getInfo: roles must be a non-null array!')
-        }
-        commit('SET_ID', userid)
-        commit('SET_ROLES', roles)
-        commit('SET_NAME', username)
-        commit('SET_AVATAR', avatar)
-        commit('SET_INTRODUCTION', introduction)
-        commit('SET_PHONE', phone)
-        commit('SET_EMAIL', email)
-        resolve(data)
-      }).catch(error => {
-        reject(error)
-      })
+      getInfo()
+        .then((response) => {
+          const { data } = response
+          if (!data) {
+            return reject('Verification failed, please Login again.')
+          }
+
+          if (!data.roles || data.roles.length <= 0) {
+            reject('getInfo: roles must be a non-null array!')
+          }
+
+          commitUserInfoToState(commit, data)
+
+          resolve(data)
+        })
+        .catch((error) => {
+          reject(error)
+        })
     })
   },
+  /*
+  getInfo({ commit, dispatch }) {
+    return new Promise((resolve, reject) => {
+      const localUserInfo = getDecryptedCookie('userInfo')
+
+      if (localUserInfo) {
+        console.log('从本地存储中读取用户信息')
+        commitUserInfoToState(commit, localUserInfo)
+
+        const wsInstance = WebSocketSingleton.getInstance(localUserInfo.userid)
+        wsInstance.addEventListener('message', (event) => {
+          const rawcommand = JSON.parse(event.data)
+          const { id, ...command } = rawcommand
+          void id
+          if (rawcommand.senderId !== localUserInfo.userid) {
+          // 使用 context.dispatch 调用 messages 模块的 action
+            dispatch('messages/saveMessage', { message: command, userId: localUserInfo.userid }, { root: true })
+          }
+        })
+
+        resolve(localUserInfo)
+      } else {
+        console.log('本地存储中没有用户信息，向后端请求')
+        getInfo()
+          .then((response) => {
+            const { data } = response
+            if (!data) {
+              return reject('Verification failed, please Login again.')
+            }
+
+            if (!data.roles || data.roles.length <= 0) {
+              reject('getInfo: roles must be a non-null array!')
+            }
+
+            commitUserInfoToState(commit, data)
+            setEncryptedCookie('userInfo', data)
+
+            const wsInstance = WebSocketSingleton.getInstance(data.userid)
+            wsInstance.addEventListener('message', (event) => {
+              const rawcommand = JSON.parse(event.data)
+              const { id, ...command } = rawcommand
+              void id
+              if (rawcommand.senderId !== data.userid) {
+              // 使用 context.dispatch 调用 messages 模块的 action
+                dispatch('messages/saveMessage', { message: command, userId: data.userid }, { root: true })
+              }
+            })
+
+            resolve(data)
+          })
+          .catch((error) => {
+            reject(error)
+          })
+      }
+    })
+  },
+  */
 
   updateUserData({ commit }, userForm) {
     return new Promise((resolve, reject) => {
@@ -134,13 +218,53 @@ const actions = {
     })
   },
 
+  getFriends({ commit }) {
+    return new Promise((resolve, reject) => {
+      getFriends().then(response => {
+        const { data } = response
+        commit('SET_FRIENDS', data)
+        resolve()
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
+  addFriends({ commit }, friend) {
+    return new Promise((resolve, reject) => {
+      addFriend(friend).then(response => {
+        const { data } = response
+        const currentFriends = state.friends || []
+        const updatedFriends = [...currentFriends, data.friend]
+        commit('SET_FRIENDS', updatedFriends)
+        resolve()
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
+  getGroups({ commit }) {
+    return new Promise((resolve, reject) => {
+      getGroups().then(response => {
+        const { data } = response
+        commit('SET_GROUPS', data)
+        resolve()
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
   // user logout
   logout({ commit, state }) {
     return new Promise((resolve, reject) => {
       logout(state.token).then(() => {
         removeToken() // must remove  token  first
+        // removeCookie('userInfo') // 删除本地存储的用户信息
         resetRouter()
         commit('RESET_STATE')
+        WebSocketSingleton.resetInstance
         resolve()
       }).catch(error => {
         reject(error)
@@ -152,6 +276,7 @@ const actions = {
   resetToken({ commit }) {
     return new Promise(resolve => {
       removeToken() // must remove  token  first
+      // removeCookie('userInfo') // 删除本地存储的用户信息
       commit('RESET_STATE')
       resolve()
     })
@@ -175,6 +300,19 @@ const actions = {
     // reset visited views and cached views
     dispatch('tagsView/delAllViews', null, { root: true })
   }
+
+}
+
+function commitUserInfoToState(commit, data) {
+  const { userid, roles, username, avatar, introduction, phone, email } = data
+
+  commit('SET_ID', userid)
+  commit('SET_ROLES', roles)
+  commit('SET_NAME', username)
+  commit('SET_AVATAR', avatar)
+  commit('SET_INTRODUCTION', introduction)
+  commit('SET_PHONE', phone)
+  commit('SET_EMAIL', email)
 }
 
 export default {
