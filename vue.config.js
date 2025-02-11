6c9a640dc7f9daebf3a2028fde589205529b55f4
videoqwg <3080926500@qq.com>
2025-02-11 17:10:30 +0800

    添加对uuid库的支持依赖

--- vue.config.js
+++ vue.config.js
@@ -24,6 +24,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
+  transpileDependencies: ['uuid'],
   publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'static',
